a51ee9ae4caa91f50c223eb8e74ac7f7feeb4cf5
Chan Jun Da <chan.j.d.98@gmail.com>
2020-06-27 03:03:42 +0800

    Minor UI improvements

--- src/main/java/BridgeGroupInterface.java
+++ src/main/java/BridgeGroupInterface.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class BridgeGroupInterface implements ViewerInterface {
 
     private IOInterface ioInterface;
@@ -16,6 +18,10 @@ public class BridgeGroupInterface implements ViewerInterface {
     private static final String RESEND_EDIT = "(Deleted: Refer to the newest message below for details)";
     private static final String NULL_STRING = "-";
 
+    private static final String HEADER = "*GAME FEED*";
+    private static final String SHORT_LINE_BREAK = "--------------------------------------------";
+    private static final String LONG_LINE_BREAK = SHORT_LINE_BREAK + SHORT_LINE_BREAK;
+
     public BridgeGroupInterface(long chatId, IOInterface ioInterface) {
         this.chatId = chatId;
         this.ioInterface = ioInterface;
@@ -25,12 +31,15 @@ public class BridgeGroupInterface implements ViewerInterface {
 
     public String toString() {
         if (bidConcluded) {
-            return String.format("%s\n*Trick Counts*: %s\n*Trick* %s",
+            return String.format("%s\n%s\n%s\n*Trick Counts*: %s\n\n*Trick* %s",
+                    HEADER + "\n" + LONG_LINE_BREAK,
                     bidString,
+                    LONG_LINE_BREAK,
                     gameState == null ? NULL_STRING : gameState,
                     currentTrick == null ? NULL_STRING : currentTrick);
         } else {
-            return String.format("*Bid*: %s", bidString == null ? NULL_STRING : bidString);
+            return String.format(HEADER + "\n" + SHORT_LINE_BREAK +
+                    "\n*Bid*: %s", bidString == null ? NULL_STRING : bidString);
         }
     }
 
@@ -57,7 +66,7 @@ public class BridgeGroupInterface implements ViewerInterface {
                 ioInterface.editMessage(chatId, messageId, this.toString());
                 break;
             case EDIT_HAND:
-                this.currentTrick = message;
+                this.currentTrick = processCurrentTrickMessage(message);
                 ioInterface.editMessage(chatId, messageId, this.toString());
                 break;
             case EDIT_STATE:
@@ -72,8 +81,11 @@ public class BridgeGroupInterface implements ViewerInterface {
 
     }
 
-    public String processGameStateMessage(String message) {
-        return message;
+    private static String processGameStateMessage(String message) {
+
+        String[] trickCounts = message.split(", ");
+        return "\n - " + String.join("\n - ", trickCounts);
+
     }
 
     public long getViewerId() {
@@ -85,6 +97,39 @@ public class BridgeGroupInterface implements ViewerInterface {
         messageId = ioInterface.sendMessageToId(chatId, this.toString());
     }
 
+
+    private static String processCurrentTrickMessage(String message) {
+        String[] temp = message.split(": ");
+
+        if (temp.length == 1) {
+            return temp[0] + ": {  }";
+        }
+
+        String turn = temp[0];
+        String cards = temp[1];
+
+        String[] cardsArray = cards.split(", ");
+
+        StringBuilder finalString = new StringBuilder();
+        finalString.append("*" + turn + "*: ");
+        finalString.append("{  ");
+        boolean first = true;
+        for (String card : cardsArray) {
+            if (first) {
+                first = false;
+            } else {
+                finalString.append(", ");
+            }
+            finalString.append("\\[ " + card + "  ]");
+        }
+        finalString.append("  }");
+        return finalString.toString();
+    }
+
+    public static void main(String[] args) {
+        String testString = "1: SA, HA";
+        System.out.println(processCurrentTrickMessage(testString));
+    }
 
 
 
